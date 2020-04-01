cf4971b61dbffcf5076ee76daaf28d2eb1626863
Jeppe Hauman <jeppe_hauman@hotmail.com>
2020-04-01 10:44:06 +0200

    Tiltøjet props - navn, adresse, postnr

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,10 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        public string navn { get; set; }
+
+        public string adresse { get; set; }
+
+        public int postnr { get; set; }
     }
 }
